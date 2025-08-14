109c5ea54bb76ba87e36889bab2331bc74557e72
Mehmet Düzoylum <duzoylummehmet@gmail.com>
2025-08-14 16:50:30 +0300

    Html edited

--- app/Policies/PostPolicy.php
+++ app/Policies/PostPolicy.php
@@ -13,7 +13,7 @@ class PostPolicy
      */
     public function viewAny(User $user): bool
     {
-        return true; // Herkes published post'ları görebilir
+        return true;
     }
 
     /**
